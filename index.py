be8d713c9e07d70b1661530fb8a530fa24ee7fd4
jukajr <carlos.azevedo.jr@gmail.com>
2022-02-23 19:27:16 -0300

    fix-login

--- index.py
+++ index.py
@@ -344,6 +344,12 @@ def login():
         #TODO mto ele da erro e poco o botao n abre
         # time.sleep(10)
 
+    if clickBtn(images['connect-login'], timeout = 10):
+        logger('🎉 Connect login button detected, logging in!')
+        login_attempts = login_attempts + 1
+        #TODO mto ele da erro e poco o botao n abre
+        # time.sleep(10)
+
     if clickBtn(images['select-wallet-2'], timeout=8):
         # sometimes the sign popup appears imediately
         login_attempts = login_attempts + 1
